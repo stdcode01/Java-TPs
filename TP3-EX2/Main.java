40a5ce476e92fdbb53b6585c454d59fbb66c2407
rachidcooder <cooderherou@gmail.com>
2023-11-29 14:48:00 +0000

    add ex2 tp3

--- TP3-EX2/Main.java
+++ TP3-EX2/Main.java
@@ -1,6 +1,18 @@
+import java.util.*;
+
 public class Main {
 
   public static void main(String args[]) {
-    System.out.println("hello Employees");
+    ArrayList<Employees> Emps = new ArrayList<>();
+
+    Employees efw1 = new PayFixWeek("efw1", "rachid", "rue 122", 1200);
+    Employees efw2 = new PayFixWeek("efw2", "ahmed", "rue 16", 1000);
+    Employees efw3 = new PayFixWeek("efw3", "hassan", "rue 76", 900);
+
+    Emps.add(efw1);
+    Emps.add(efw1);
+    Emps.add(efw2);
+
+    System.out.println(" salary of emp1 : " + Emps.get(0).getSalary() + " per week .");
   }
 }
